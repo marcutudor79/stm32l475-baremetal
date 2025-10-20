edcf8c70ca059a94b3ba69f9679f4da614aee668
marcutudor79 <marcutudor5@protonmail.com>
2025-10-20 14:34:08 +0200

    Improve dummy app with fibonnaci computation
    - Add both linear and recursive ways
    - Compare the stack usage
    
    Signed-off-by: marcutudor79 <marcutudor5@protonmail.com>

--- src/main.c
+++ src/main.c
@@ -1,4 +1,42 @@
-int main(void) 
+int n = 0U;
+
+// recursive fibo_recursivennaci - not good in embedded apps due to being susceptible to stack overflow
+int fibo_recursive(int n)
 {
+    if (n <= 1U)
+        return n;
+
+    return (fibo_recursive(n-1) + fibo_recursive(n-2));
+}
+
+int fibo_linear(int n)
+{
+    int a = 0U;
+    int b = 1U;
+
+    if (n <= 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    for (int i = 2; i <= n; ++i)
+    {
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
+int main(void)
+{
+    if (fibo_recursive(3) != fibo_linear(3))
+    {
+        // error occured
+        while(1);
+    }
+
+    // worked fine
     while(1);
 }
